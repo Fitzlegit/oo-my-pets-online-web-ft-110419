db0b66dbc9b6884f2128e60a1bad2348b4da029b
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-01 03:10:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
   # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+  end
+
+  def self.mood
+    @mood
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << save
+  end
+
+
 end
