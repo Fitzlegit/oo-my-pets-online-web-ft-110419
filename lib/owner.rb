f0b82db926f9b8b1da76dd54c678962f44886d1a
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-01 03:47:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,7 +62,8 @@ class Owner
   end
 
   def sell_pets
-    self.list_pets
+    self.dogs.each { |dog| dog.mood = "nervous" }
+    self.cats.each { |cat| cat.mood = "nervous" }
   end
 
   def list_pets
